2e6ace62a66110915bf7c79341ab6cc9bd7aa919
антон сизов <aantonssizov@gmail.com>
2021-08-13 19:21:11 +0300

    Add project files.

---
+++ ConfigHomework/AccountConfig.cs
@@ -0,0 +1,9 @@
+﻿namespace ConfigHomework
+{
+    public class AccountConfig
+    {
+        public bool IsPremium { get; set; }
+        public float Balance { get; set; }
+        public int Level { get; set; }
+    }
+}
---
+++ ConfigHomework/UserConfig.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConfigHomework
+{
+    public class UserConfig
+    {
+        public string Name { get; set; }
+        public int Phone { get; set; }
+        public bool IsActive { get; set; }
+        public AccountConfig AccountConfig { get; set; }
+    }
+}
